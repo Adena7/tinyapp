025722c9da96a837db8f1c681363b9d599f5fc51
Abdoullahi Aden <abdoullahi.aden@gmail.com>
2021-12-07 22:31:40 +0000

    redirecting short URL to the long URL

--- express_server.js
+++ express_server.js
@@ -37,8 +37,17 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-    console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = req.body.longURL;
+    res.redirect(`urls/${shortURL}`)
+  });
+  
+  app.get("/u/:shortURL", (req, res) => {
+    shortURL = req.params.shortURL;
+    if(urlDatabase.hasOwnProperty(shortURL)) {
+      let longURL = urlDatabase[shortURL];
+      res.redirect(`${longURL}`);
+    };
 });
 
 app.get("/", (req, res) => {
