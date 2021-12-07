add4df4442d32c88adf64cb93f862da7e8a8e5f9
Abdoullahi Aden <abdoullahi.aden@gmail.com>
2021-12-07 21:29:27 +0000

    GET Route to Show the Form

--- express_server.js
+++ express_server.js
@@ -14,6 +14,10 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
     const long = urlDatabase[req.params.shortURL];
     const templateVars = { shortURL: req.params.shortURL, longURL: long} ;
